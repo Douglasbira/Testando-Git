d7df1d8533596ee294078074be1f53001c64253f
Douglas Francisco <douglasfsilvag@gmail.com>
2021-03-07 10:12:57 -0300

    Add action in layout

--- app/src/main/java/com/biradev/testandogit/MainActivity.java
+++ app/src/main/java/com/biradev/testandogit/MainActivity.java
@@ -3,12 +3,40 @@ package com.biradev.testandogit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Text;
+
+    private Button Button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Text = findViewById(R.id.textView2);
+        Button = findViewById(R.id.button2);
+
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Oi Douglas", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
